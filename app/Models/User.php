d4704a62c3b5adeaf5b0abab366faf2e01f9b721
RomanKois <koisroman@gmail.com>
2023-05-02 18:20:25 +0200

    implemets basic user fucntionality with role protection

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,40 +5,34 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    protected $table = "user";
+
     protected $fillable = [
-        'name',
-        'email',
+        "name",
+        "email",
         'password',
+        "role"
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Middleware\UserCapability;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\api\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+//Users
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'logIn']);
+//delete after deply
+Route::post('createadmin', [UserController::class, 'createSuperUser']);
+
+Route::get('users', [UserController::class, 'getAllUsers'])->middleware('role:admin');
+Route::post('changerole', [UserController::class, 'changeRole'])->middleware('role:admin');
+Route::post('deleteuser', [UserController::class, 'deleteUser'])->middleware('role:admin');
+
+
+
+
+
+// Route::get('users', [UserController::class, 'getAllUsers']);
