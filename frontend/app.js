b2639b28cc595772001f523a10c44ac5415c574f
RomanKois <koisroman@gmail.com>
2023-05-02 14:38:33 +0200

    fix setup

---
+++ frontend/app.js
@@ -0,0 +1,16 @@
+import '../resources/js/bootstrap'
+import { createApp } from 'vue'
+
+import App from './app/App.vue'
+import store from './plugins/store'
+import vuetify from './plugins/vuetify'
+import router from './plugins/router'
+import i18n from './plugins/i18n'
+
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.use(vuetify)
+app.use(i18n)
+
+app.mount("#app")
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+// import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [
     vue(),
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
+    // laravel({
+    //   input: ['resources/css/app.css', 'resources/js/app.js'],
+    //   refresh: true,
+    // }),
   ],
   build: {
     define: {
@@ -16,4 +16,5 @@ export default defineConfig({
       '__VUE_I18N_LEGACY_API__': false,
     },
   },
+  root: './frontend'
 });
