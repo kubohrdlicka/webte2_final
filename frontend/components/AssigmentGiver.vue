68838813e48f6b496d49669b6e1b14da142a6bf6
RomanKois <koisroman@gmail.com>
2023-05-05 14:00:16 +0200

    img into math

--- frontend/components/AssigmentGiver.vue
+++ frontend/components/AssigmentGiver.vue
@@ -1,29 +1,64 @@
 <template>
     <div v-html="htmlContent"></div>
-  </template>
+</template>
   
-  <script>
-  import { HtmlGenerator, parse } from 'latex.js'
-  
-  export default {
+<script>
+import { HtmlGenerator, parse } from 'latex.js'
+
+
+
+export default {
     name: 'AssigmentGiver',
     data() {
-      return {
-        assigment: `Nájdite prenosovú funkciu $F(s)=\\dfrac{Y(s)}{W(s)}$ pre systém opísaný blokovou schémou: 
-                    \\includegraphics[width= 650, height=500]{/sk.png}`,
-      };
+        return {
+            assigment: `Nájdite prenosovú funkciu $F(s)=\\dfrac{Y(s)}{W(s)}$ pre systém opísaný blokovou schémou: 
+                    \\includegraphics{/sk.png}`,
+        };
     },
     computed: {
-      htmlContent() {
-        this.assigment = `\\usepackage{graphicx}\n` + `\\begin{document}\n`+this.assigment+`\n\\end{document}`;
-        const latex = new HtmlGenerator({ hyphenate: false });
-        let html = parse(this.assigment, {generator: latex});
-        html = html.domFragment();
-        // latex.loadPackage('base');
-        // const html = latex.compile(this.texContent).toHTML();
-        return html.firstChild.outerHTML;
-      },
+        htmlContent() {
+            this.assigment = this.assigment;
+            const latex = new HtmlGenerator({
+                hyphenate: false,
+                CustomMacros: (function () {
+                    function CustomMacros(generator) {
+                        this.g = generator
+                    }
+
+                    var args = CustomMacros.args = {},
+                        prototype = CustomMacros.prototype
+
+
+                    args['includegraphics'] = ['H', 'k']
+                    
+                    prototype['includegraphics'] = function (src) {
+                        let img =  this.g.create('img')
+                        img.src = src
+                        img.className = 'img-fluid'
+                        return [img]
+                    }
+                    
+                    
+                    return CustomMacros
+                }())
+            });
+
+            let html = parse(this.assigment, { generator: latex });
+            html = html.domFragment();
+
+            return html.firstChild.outerHTML;
+        },
     },
-  };
-  </script>
+};
+</script>
   
+
+<style>
+
+.img-fluid {
+    max-width: 60%;
+    height: auto;
+    margin: 0 auto;
+}
+
+</style>
