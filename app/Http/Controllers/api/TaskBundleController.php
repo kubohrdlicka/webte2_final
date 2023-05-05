a25d66aa7354548b974aa7793f4cf803b3a30093
RomanKois <koisroman@gmail.com>
2023-05-05 22:01:52 +0200

    implements .zip upload

--- app/Http/Controllers/api/TaskBundleController.php
+++ app/Http/Controllers/api/TaskBundleController.php
@@ -11,19 +11,34 @@ class TaskBundleController extends Controller
 {
     public function uploadTask(Request $request)
     {
-        $file = $request->file('file');
+        $file = $request->file('zipFile');
 
-        
-        
-        $taskbunle = TaskBundle::create([
-            'name' => $file->getClientOriginalName(),
-            'description' => $request->description,
-        ]);
-        
-        $id = $taskbunle->id;
-        
-        $this->parseTaskBundle($file, $id);
 
+        $tempPath = $file->store('temp', 'local');
+
+        $zip = new ZipArchive;
+
+        $zip->open(storage_path('app/' . $tempPath));
+
+        for ($i = 0; $i < $zip->numFiles; $i++) {
+            $filename = $zip->getNameIndex($i);
+
+            if (pathinfo($filename, PATHINFO_EXTENSION) == 'tex') {
+                $taskbunle = TaskBundle::create([
+                    'name' => $file->getClientOriginalName(),
+                    'description' => $request->description,
+                ]);
+                $id = $taskbunle->id;
+                $this->parseTaskBundle($file, $id);
+            } else {
+                $publicPath = public_path();
+                $fileDestination = $publicPath . '/' . basename($filename);
+                copy("zip://" . storage_path('app/' . $tempPath) . "#" . $filename, $fileDestination);
+            }
+        }
+
+        $zip->close();
+        Storage::delete($tempPath);
 
         // $file->move('../../../public/latex', $file->getClientOriginalName());
 
@@ -34,9 +49,9 @@ class TaskBundleController extends Controller
 
     private function parseTask($file, $id)
     {
-        
-    // $latex = file('./blokovka01pr.tex');
-    //    var_dump($latex);
+
+        // $latex = file('./blokovka01pr.tex');
+        //    var_dump($latex);
 
         $latex = $file;
 
@@ -46,56 +61,56 @@ class TaskBundleController extends Controller
         $names = [];
         $assigments = [];
         $solutions = [];
-    
-    
-        foreach ($latex as $line){
-            if(strpos($line, '\section*{') !== false){
+
+
+        foreach ($latex as $line) {
+            if (strpos($line, '\section*{') !== false) {
                 $names[] = $name;
-                $name = substr($line, strpos($line, '{')+1,-2);
+                $name = substr($line, strpos($line, '{') + 1, -2);
             }
 
-            if($breaker && !(strpos($line, '\end{solution}') !== false || strpos($line, '\end{task} ') !== false) ){
+            if ($breaker && !(strpos($line, '\end{solution}') !== false || strpos($line, '\end{task} ') !== false)) {
                 $content .= $line;
             }
-    
-            if(strpos($line, '\begin{task}') !== false){
+
+            if (strpos($line, '\begin{task}') !== false) {
                 $breaker = true;
                 //$content .= $line;
             }
-    
-    
-            if(strpos($line, '\end{task} ') !== false){
+
+
+            if (strpos($line, '\end{task} ') !== false) {
                 $assigments[] = $content;
-                $breaker =false;
+                $breaker = false;
                 $content = '';
             }
-    
-            if(strpos($line, '\begin{solution}') !== false){
-    //            $content .= $line;
+
+            if (strpos($line, '\begin{solution}') !== false) {
+                //            $content .= $line;
                 $breaker = true;
             }
-    
-            if(strpos($line, '\end{solution}') !== false ){
+
+            if (strpos($line, '\end{solution}') !== false) {
                 $breaker = false;
                 $solutions[] = $content;
                 $content = '';
             }
-    
-    
+
+
         }
-    
+
         $done = [];
-    
-        for($i = 0; $i < sizeof($solutions); $i++){
-                Task::create([
-                    'name' => $names[$i],
-                    'instructions' => $assigments[$i],
-                    'solution' => $solutions[$i],
-                    'task_bundle_id' => $id
-                ]);
+
+        for ($i = 0; $i < sizeof($solutions); $i++) {
+            Task::create([
+                'name' => $names[$i],
+                'instructions' => $assigments[$i],
+                'solution' => $solutions[$i],
+                'task_bundle_id' => $id
+            ]);
         }
-        
-    
+
+
 
     }
 }
